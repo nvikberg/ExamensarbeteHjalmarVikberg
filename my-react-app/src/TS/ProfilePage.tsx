f5120e43c86c70b302e6a0c89bd9cc45d91e73f1
lohjal001 <113910139+lohjal001@users.noreply.github.com>
2025-01-08 21:47:37 +0100

    Start of adding name

--- my-react-app/src/TS/ProfilePage.tsx
+++ my-react-app/src/TS/ProfilePage.tsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import { db } from '../Data/firebase';
-import { getAuth } from 'firebase/auth';
-import { getDoc, collection, query, where, doc } from 'firebase/firestore';
+import { getAuth, updateCurrentUser } from 'firebase/auth';
+import { getDoc, collection, query, where, doc, updateDoc, setDoc } from 'firebase/firestore';
 import styles from '../CSS/ProfilePage.module.css';
-import logoutStyles from '../CSS/Logout.module.css'
 import DeleteUser from './DeleteUser';
 import Logout from './Logout';
 
 interface User {
+    uid: string;
     email: any;
+    fName: string;
+    lName: string;
     created: any;
     profilePic?: string;
     bio?: string;
 }
 
 const ProfilePage: React.FC = ({ }) => {
+    const [newFirstName, setNewListTitle] = useState('');
+    const [newLastName, setNewLastName] = useState('');
     const [user, setUser] = useState<User | null>(null);
+    const [successMessage, setSuccessMessage] = useState<string>('')
     const [loading, setLoading] = useState<boolean>(true); // Add loading state
     const auth = getAuth();
     const currentUser = auth.currentUser;
@@ -51,9 +56,43 @@ const ProfilePage: React.FC = ({ }) => {
         return date.toLocaleDateString();
     };
 
-    //   if (loading) {
-    //     return <div>Loading...</div>;
-    //   }
+    async function handleSaveName() {
+        if (!newFirstName.trim() && newLastName.trim()) {
+            setSuccessMessage(`Please enter your name`);
+            setTimeout(() => setSuccessMessage(''), 3000);
+            return;
+        }
+
+        setLoading(true);
+        try {    
+            if (!currentUser || !currentUser.uid) {
+                throw new Error("User not logged in.");
+            }        
+            const userRef = doc(db, "Users", currentUser.uid);
+            const userSnap = await getDoc(userRef);
+            
+            if (userSnap.exists()){
+                await updateDoc(userRef, {
+                    firstName: newFirstName.trim(),
+                    lastName: newLastName.trim(),
+                });
+            }else {
+                await setDoc(userRef, {
+                    firstName: newFirstName.trim(),
+                    newLastName: newLastName.trim(),
+                })
+            }
+            setSuccessMessage('Name updated successfully!');
+            setTimeout(() => {
+                setSuccessMessage('');
+            }, 3000);
+        } catch (error) {
+            console.error('Error updating name:', error);
+            setSuccessMessage('An error occurred. Please try again.');
+        } finally {
+            setLoading(false);
+        }
+    }
 
     return (
         <>
@@ -73,6 +112,12 @@ const ProfilePage: React.FC = ({ }) => {
 
                 <div className={styles.profileDetails}>
                     <h3>Contact Information</h3>
+                    <form className={styles.saveNameForm}>
+                        <p>Add your name to your profile:</p>
+                        <input type="text" value={newFirstName} placeholder='Firstname' />
+                        <input type="text" value={newLastName} placeholder='Lastname' />
+                        <button onClick={handleSaveName}>Save name</button>
+                    </form>
                     <h4 className={styles.toDo}>Att göra i kod - Lägga till så användare kan ändra email och lösen?  </h4>
                     <p><strong>Your Email</strong> {user?.email}</p>
                     <p><strong>Your account was created on</strong> {user?.created && formatDate(user.created)}</p>
